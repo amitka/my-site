7c29f2ef40710d8bbabb8458cf43db0f65f391b3
Amit Kaplinsky <amitka@Amitka-UX-MBP.local>
2020-05-25 16:45:26 +0300

    Updates

---
+++ precache-manifest.17374a58afbc30eac11eaf045eac709f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68247579acd9c86a577f82d77a049f49",
+    "url": "/my-site/index.html"
+  },
+  {
+    "revision": "7562d0f41da625adc1a7",
+    "url": "/my-site/static/css/main.4a2a5165.chunk.css"
+  },
+  {
+    "revision": "38c1a49b316c74dc7774",
+    "url": "/my-site/static/js/2.27993f6a.chunk.js"
+  },
+  {
+    "revision": "9a2cf355ca348d84eedb0129142dc576",
+    "url": "/my-site/static/js/2.27993f6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7562d0f41da625adc1a7",
+    "url": "/my-site/static/js/main.18315b89.chunk.js"
+  },
+  {
+    "revision": "6b68daa2a4f5dc5279ba",
+    "url": "/my-site/static/js/runtime-main.d1dfb7da.js"
+  },
+  {
+    "revision": "c023577bf9f4552982e7ba70d363e3f4",
+    "url": "/my-site/static/media/HomeTabConcepts.c023577b.jpg"
+  },
+  {
+    "revision": "3ed4bf415e9680cf305b09ea61d6f7e1",
+    "url": "/my-site/static/media/InitialConcept.3ed4bf41.jpg"
+  },
+  {
+    "revision": "1f914f9e642cec0a25e57a0b28f53b72",
+    "url": "/my-site/static/media/ItemPage.1f914f9e.jpg"
+  },
+  {
+    "revision": "7149fdae1cce88366d48f269ebedcc42",
+    "url": "/my-site/static/media/OnBoarding.7149fdae.jpg"
+  },
+  {
+    "revision": "ccb8d7aacf46c8c0eb2cb9fd4ec3c078",
+    "url": "/my-site/static/media/OtherVendors.ccb8d7aa.png"
+  },
+  {
+    "revision": "d639cc2022373d6982d1eb3ff99a39ef",
+    "url": "/my-site/static/media/SideDrawerNavConcepts.d639cc20.jpg"
+  },
+  {
+    "revision": "b3c3d373b8b2869b1422ca07e41a20c4",
+    "url": "/my-site/static/media/hole.b3c3d373.jpeg"
+  },
+  {
+    "revision": "0236e20c2624a3401acf3d1b83ece819",
+    "url": "/my-site/static/media/packShot.0236e20c.jpg"
+  },
+  {
+    "revision": "d459aa13f8e54a1a3b689718deb04007",
+    "url": "/my-site/static/media/watch-tower.d459aa13.gif"
+  }
+]);
